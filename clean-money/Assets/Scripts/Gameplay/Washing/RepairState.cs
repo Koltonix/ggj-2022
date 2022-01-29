d5937696540160b952090436144f0e2fb23c1677
CHRISTOPHER ROBERTSON <CR230727@falmouth.ac.uk>
2022-01-29 13:43:28 +0000

    - Continued the Repair aspect of the washing machine.
    - Imported TMPro;

--- clean-money/Assets/Scripts/Gameplay/Washing/RepairState.cs
+++ clean-money/Assets/Scripts/Gameplay/Washing/RepairState.cs
@@ -7,11 +7,26 @@ namespace cm.gameplay
 {
     public class RepairState : MachineState
     {   
-
         [SerializeField]
         private KeyCode repairKey = KeyCode.E;
 
-       protected override void CountDown() 
+        [SerializeField]
+        private int hitsRequired = 5;
+        [SerializeField]
+        private int hitsLeft = 0;
+
+        [SerializeField]
+        private IntEvent onHitValue = null;
+
+        public override void OnStateEnter()
+        {
+            base.OnStateEnter();
+
+            hitsLeft = hitsRequired;
+            onHitValue?.Invoke(hitsLeft);
+        }
+
+        protected override void CountDown() 
         {
             return;
         }
@@ -20,7 +35,13 @@ namespace cm.gameplay
         {
             PlayerMovement player = col.gameObject.GetComponent<PlayerMovement>();
             if (Input.GetKeyDown(repairKey) && player && canOccur)
-                onTimerEnd?.Invoke();
+            {
+                hitsLeft--;
+                onHitValue?.Invoke(hitsLeft);
+                if (hitsLeft <= 0)
+                    onTimerEnd?.Invoke();
+            }
+                
         }
     }
 }
